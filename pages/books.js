5235fa8bd52f3ec23f26cb6b16fdc2c21448cfd4
sasidhar-cell <sasidhar.joga23@gmail.com>
2026-01-21 18:20:54 +0530

    Add Books component to display academic textbooks

---
+++ pages/books.js
@@ -0,0 +1,8 @@
+export default function Books() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Books</h1>
+      <p>Academic textbooks will appear here.</p>
+    </div>
+  );
+}
